5446288ffe4dcbc465cf73d80af6333d7b84d541
@vbandrade <vbandrade@gmail.com>
2018-03-02 01:45:32 -0300

    criado Overlay de resposta correta

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -4,6 +4,7 @@ import "../utils/questions.dart";
 import "../utils/quiz.dart";
 import "../UI/answer_button.dart";
 import "../UI/question_text.dart";
+import "../UI/correct_wrong_overlay.dart";
 
 class QuizPage extends StatefulWidget {
   @override
@@ -14,6 +15,7 @@ class QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return new Stack(
+      fit: StackFit.expand,
       children: <Widget>[
         new Column(
           // main page
@@ -22,7 +24,8 @@ class QuizPageState extends State<QuizPage> {
             new QuestionText("Pizza is nice", 1),
             new AnswerButton(false, () => print("false")),
           ],
-        )
+        ),
+        new CorrectWrongOverlay()
       ],
     );
   }
