eda6ba63c035fd4c35c41dd7b2c30cb3144bb21c
@vbandrade <vbandrade@gmail.com>
2018-03-01 22:04:06 -0300

    exibindo texto na tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import "./pages/landingPage.dart";
+import "./pages/landing_page.dart";
 
 void main() => runApp(new MaterialApp(
       home: new LandingPage(),
--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -3,6 +3,16 @@ import "package:flutter/material.dart";
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Material(color: Colors.amber);
+    return new Material(
+        color: Colors.amber,
+        child: new InkWell(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text("Lets Quizzz"),
+              new Text("Tap to Start!")
+            ],
+          ),
+        ));
   }
 }
